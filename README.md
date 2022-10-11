e82cae7c1568298a68b2f38b9c2a41a301b8109b
Bodo Schulz <bodo@boone-schulz.de>
2022-10-11 12:37:45 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# monitoring check for redis
+
+## usage
+
+```
+./check_redis.py --help
+usage: check_redis.py [-h] [-H HOST] [-p PORT] [-P PASSWORD] [-d DBNAME] [-t TIMEOUT] [-w WARNING] [-c CRITICAL] [-k KEY_VALUE]
+
+monitoring plugin for redis-server
+
+options:
+  -h, --help            show this help message and exit
+  -H HOST, --host HOST  Redis server to connect to. (default is 127.0.0.1)
+  -p PORT, --port PORT  Redis port to connect to. (default is 6379)
+  -P PASSWORD, --password PASSWORD
+                        Redis password to connect to.
+  -d DBNAME, --dbname DBNAME
+                        Redis database name (default is db0)
+  -t TIMEOUT, --timeout TIMEOUT
+                        Number of seconds to wait before timing out and considering redis down
+  -w WARNING, --warning WARNING
+                        Warning threshold.
+  -c CRITICAL, --critical CRITICAL
+                        Critical threshold.
+  -k KEY_VALUE, --key KEY_VALUE
+                        Stat to monitor (memory_mb, hit_ratio, or custom)
+```
+
+## examples
+
+```
+./check_redis.py
+OK REDIS version: 7.0.5, connected_clients: 1, db0: 1, used_memory_human: 672.77K, uptime_in_days: 2
+```
+
+```
+./check_redis.py --key hit_ratio --critical 0 --warning 1
+OK: Redis hit_ratio is 0 || hit_ratio=0;1;0;0;0
+```
+
+```
+./check_redis.py --key memory_mb --critical 1 --warning 3
+OK: Redis memory_mb is 8.98046875 || memory_mb=8.98046875;3;1;0;8.98046875
+```
---
+++ check_redis.py
@@ -0,0 +1,209 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import redis
+import sys
+import argparse
+
+EXIT_OK = 0
+EXIT_WARNING = 1
+EXIT_CRITICAL = 2
+EXIT_UNKNONW = 3
+EXIT_INVALID_AUTH = 3
+
+
+class MonitoringPluginRedis(object):
+
+    def __init__(self):
+        """
+        """
+        cli_args = self.parse_args()
+
+        self.host = cli_args.host
+        self.port = cli_args.port
+        self.password = cli_args.password
+        self.dbname = cli_args.dbname
+        self.timeout = cli_args.timeout
+        self.key = cli_args.key_value
+        self.warning = cli_args.warning
+        self.critical = cli_args.critical
+
+        try:
+            self.conn = redis.Redis(
+                host=self.host,
+                port=self.port,
+                password=self.password,
+                socket_timeout=self.timeout
+            )
+            self.info_out = self.conn.info()
+            self.conn.ping()
+
+        except Exception as e:
+            print(f"CRITICAL REDIS : {e}")
+            sys.exit(2)
+
+    def parse_args(self):
+        """
+        """
+        parser = argparse.ArgumentParser(
+            description="monitoring plugin for redis-server, version: 1.0"
+        )
+        parser.add_argument(
+            "-H", "--host",
+            dest="host",
+            help="Redis server to connect to. (default is 127.0.0.1)",
+            default="127.0.0.1"
+        )
+        parser.add_argument(
+            "-p", "--port",
+            dest="port",
+            help="Redis port to connect to. (default is 6379)",
+            type=int,
+            default=6379
+        )
+        parser.add_argument(
+            "-P", "--password",
+            dest="password",
+            help="Redis password to connect to.",
+            default=''
+        )
+        parser.add_argument(
+            "-d", "--dbname",
+            dest="dbname",
+            help="Redis database name (default is db0)",
+            default='db0'
+        )
+        parser.add_argument(
+            "-t", "--timeout",
+            dest="timeout",
+            help="Number of seconds to wait before timing out and considering redis down",
+            type=int,
+            default=2
+        )
+        parser.add_argument(
+            "-w", "--warning",
+            dest="warning",
+            type=int,
+            help="Warning threshold."
+        )
+        parser.add_argument(
+            "-c", "--critical",
+            dest="critical",
+            type=int,
+            help="Critical threshold."
+        )
+        parser.add_argument(
+            "-k", "--key",
+            dest="key_value",
+            help="Stat to monitor (memory_mb, hit_ratio, or custom)",
+            default=None
+        )
+
+        return parser.parse_args()
+
+    def get_version(self):
+
+        return f"version: {self.info_out.get('redis_version')}"
+
+    def get_client_connection(self):
+
+        return f"connected_clients: {self.info_out.get('connected_clients')}"
+
+    def get_number_keys(self):
+
+        return f"{self.dbname}: {self.info_out.get(self.dbname).get('keys')}"
+
+    def get_uptime(self):
+
+        return f"uptime_in_days: {self.info_out.get('uptime_in_days')}"
+
+    def get_used_memory(self):
+
+        return f"used_memory_human: {self.info_out.get('used_memory_human')}"
+
+    def check(self):
+        """
+        """
+        number_keys = ''
+        version = self.get_version()
+        client_connected = self.get_client_connection()
+        reverse_check = False
+        exit_string = "OK"
+
+        if self.dbname in str(self.info_out):
+            number_keys = self.get_number_keys()
+
+        memory = self.get_used_memory()
+        uptime = self.get_uptime()
+
+        # print(self.info_out)
+
+        if self.key:
+            if not self.warning or not self.critical:
+                exit_string = "UNKNOWN"
+
+                if not self.warning:
+                    status = "UNKNOWN: Warning level required"
+                if not self.critical:
+                    status = "UNKNONW: Critical level required"
+
+                print(status)
+                sys.exit(EXIT_UNKNONW)
+
+            if self.key == "memory_mb":
+                reverse_check = True
+                info_value = int(
+                    self.info_out.get("used_memory_rss") or self.info_out.get("used_memory")
+                ) / (1024 * 1024)
+            elif self.key == "hit_ratio":
+                reverse_check = False
+                hit = int(self.info_out.get("keyspace_hits"))
+                miss = int(self.info_out.get("keyspace_misses"))
+
+                if hit > 0 and miss > 0:
+                    info_value = int(100 * hit) / (hit + miss)
+                else:
+                    info_value = 0
+            else:
+                info_value = int(self.info_out.get(self.key))
+
+            if reverse_check:
+                if int(info_value) < int(self.critical):
+                    exit_string = "CRITICAL"
+                elif int(info_value) < int(self.warning):
+                    exit_string = "WARNING"
+            else:
+                if int(info_value) > int(self.critical):
+                    exit_string = "CRITICAL"
+                elif int(info_value) > int(self.warning):
+                    exit_string = "WARNING"
+
+            status = f"{exit_string}: Redis {self.key} is {info_value}"
+            perfdata = f"{self.key}={info_value};{self.warning};{self.critical};0;{info_value}"
+
+            print(f"{status} || {perfdata}")
+
+        else:
+
+            if number_keys == '':
+                status = f"OK REDIS No keys, {version}, {memory}, {uptime}"
+            else:
+                status = f"OK REDIS {version}, {client_connected}, {number_keys}, {memory}, {uptime}"
+
+            print(status)
+
+        if exit_string == "OK":
+            sys.exit(EXIT_OK)
+        if exit_string == "WARNING":
+            sys.exit(EXIT_WARNING)
+        if exit_string == "UNKNONW":
+            sys.exit(EXIT_UNKNONW)
+        else:
+            sys.exit(EXIT_CRITICAL)
+
+
+if __name__ == "__main__":
+    """
+    """
+    server = MonitoringPluginRedis()
+    server.check()
